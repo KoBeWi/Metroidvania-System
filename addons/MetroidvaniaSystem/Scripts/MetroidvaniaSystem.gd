14d876cd5c4fe4317b6f7714cc35f60cfb3395ce
kobewi <kobewi4e@gmail.com>
2023-06-07 00:39:13 +0200

    Thread fixes (workable edition)

--- addons/MetroidvaniaSystem/Scripts/MetroidvaniaSystem.gd
+++ addons/MetroidvaniaSystem/Scripts/MetroidvaniaSystem.gd
@@ -31,6 +31,7 @@ enum { R, D, L, U }
 ## TODO: do szukania: wymyślić jakoś jak wyświetlać różne ikonki w zależności od danych
 ## TODO: summary: wypisywać ilość dla każdego rodzaju
 ## TODO: handler może się rejestrować dla aktualnej scene (owner.set_meta(&"handler"))
+## TODO: motyw pluginu (kolor podświetleń itp)
 
 @export var exported_settings: Resource
 
