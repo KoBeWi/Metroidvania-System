9ab86e2ff9c1e6f98df356104813cac8f5dc03e4
MattKalt <93488504+MattKalt@users.noreply.github.com>
2024-08-08 12:12:40 -0400

    Sublte tweaks to movement in sample
    Added Coyote time, short hops, acceleration, and max fall speed
    
    All changes are very subtle and keep the sample project simple, but make it feel slightly more polished

--- SampleProject/Scripts/Player.gd
+++ SampleProject/Scripts/Player.gd
@@ -1,8 +1,13 @@
 # This script is based on the default CharacterBody2D template. Not much interesting happening here.
 extends CharacterBody2D
 
-const SPEED = 300.0
+const SPEED_MIN = 300.0
+const SPEED_MAX = 400.0
+const ACCEL = 50.0
 const JUMP_VELOCITY = -450.0
+const MAX_FALL_SPEED = 900.0
+const COYOTE_TIME: float = .1
+const SHORT_HOP: float = .5
 
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 var animation: String
@@ -14,6 +19,8 @@ var event: bool
 var abilities: Array[StringName]
 var double_jump: bool
 var prev_on_floor: bool
+var airtime: float = 0
+var speed: float = SPEED_MIN
 
 func _ready() -> void:
 	on_enter()
@@ -23,13 +30,16 @@ func _physics_process(delta: float) -> void:
 		return
 	
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y = min(velocity.y + gravity * delta, MAX_FALL_SPEED)
+		airtime += delta
 	elif not prev_on_floor and &"double_jump" in abilities:
 		# Some simple double jump implementation.
 		double_jump = true
+		airtime = 0
 	
-	if Input.is_action_just_pressed("ui_accept") and (is_on_floor() or double_jump):
-		if not is_on_floor():
+	var on_floor_ct: bool = is_on_floor() or airtime < COYOTE_TIME
+	if Input.is_action_just_pressed("ui_accept") and (on_floor_ct or double_jump):
+		if not on_floor_ct:
 			double_jump = false
 		
 		if Input.is_action_pressed("ui_down"):
@@ -37,11 +47,21 @@ func _physics_process(delta: float) -> void:
 		else:
 			velocity.y = JUMP_VELOCITY
 	
+	if Input.is_action_just_released("ui_accept"):
+		if not is_on_floor() and velocity.y < 0:
+			velocity.y = min(0, velocity.y - JUMP_VELOCITY * SHORT_HOP)
+			
+	
+	if is_on_wall():
+		speed = SPEED_MIN
+	
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		speed = min(SPEED_MAX, speed + ACCEL * delta)
+		velocity.x = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, SPEED_MIN)
+		speed = SPEED_MIN
 
 	prev_on_floor = is_on_floor()
 	move_and_slide()
