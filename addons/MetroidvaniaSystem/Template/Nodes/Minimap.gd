50d06e7f7b3a24e55cb6e70b534ad39313d56451
kobewi <kobewi4e@gmail.com>
2024-12-25 15:22:09 +0100

    Final final

--- addons/MetroidvaniaSystem/Template/Nodes/Minimap.gd
+++ addons/MetroidvaniaSystem/Template/Nodes/Minimap.gd
@@ -45,7 +45,8 @@ var _map_view: MapView
 var _player_location: Node2D
 
 func _ready() -> void:
-	smooth_scroll = smooth_scroll and MetSys.settings.theme.show_exact_player_location
+	if not Engine.is_editor_hint():
+		smooth_scroll = smooth_scroll and MetSys.settings.theme.show_exact_player_location
 	
 	var actual_size := area
 	if smooth_scroll:
